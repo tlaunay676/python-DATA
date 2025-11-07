3984166a862b16b77af08a268caaf4a3e7827c43
tlaunay676 <thibaut.launay@ensae.fr>
2025-11-07 10:34:54 +0000

    Réorganisation des données + généralisation du scraping

--- scrapping.py
+++ scrapping.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 BASE_URL = "https://xmart-api-public.who.int/DEX_CMS/"
 
-def get_headers(grp="SDG3", loc_type="COUNTRY", version="2025"):
+def get_headers(grp, loc_type="COUNTRY", version="2025"):
     url = (f"{BASE_URL}WHSA_HEADER?"
            f"$filter=14 eq 14 and IND_GRP_CODE eq '{grp}' and LOCATION_TYPE_CODE eq '{loc_type}' and VERSION_CODE eq '{version}'"
            f"&$orderby=SORT asc")
@@ -11,7 +11,7 @@ def get_headers(grp="SDG3", loc_type="COUNTRY", version="2025"):
     resp.raise_for_status()
     return pd.DataFrame(resp.json()["value"])
 
-def get_facts(grp="SDG3", loc_type="COUNTRY", version="2025"):
+def get_facts(grp, loc_type="COUNTRY", version="2025"):
     url = (f"{BASE_URL}WHSA_FACT_DISPLAY?"
            f"$filter=14 eq 14 and IND_GRP_CODE eq '{grp}' and LOCATION_TYPE_CODE eq '{loc_type}' and VERSION_CODE eq '{version}'"
            f"&$orderby=LOCATION_SORT asc")
@@ -43,10 +43,12 @@ def build_wide_table(headers_df, facts_df):
     return wide_df
 
 if __name__ == "__main__":
-    headers_df = get_headers()
-    facts_df = get_facts()
-    table_df = build_wide_table(headers_df, facts_df)
-    print(table_df.head())
+    for grp in ("SDG3", "SDG_GPW"):
+        headers_df = get_headers(grp)
+        facts_df = get_facts(grp)
+        table_df = build_wide_table(headers_df, facts_df)
+        # Sauvegarder
+        table_df.to_csv(f"WHO_'{grp}'_COUNTRY_2025.csv", index=False)
+    print(table_df.columns)
 
-    # Sauvegarder
-    table_df.to_csv("WHO_SDG3_COUNTRY_2025.csv", index=False)
+    
