3d77d2987d5ad476df554a35b518be61911d6ef4
tlaunay676 <thibaut.launay@ensae.fr>
2025-12-24 10:23:14 +0000

    .

--- Main/desc_indicateur.py
+++ Main/desc_indicateur.py
@@ -8,10 +8,8 @@ def desc_missing(df, var, country_col):
 
     # Missing values
     n_missing = df[var].isna().sum()
-    n_obs = n_countries - n_missing
     pct_missing = (n_missing / n_countries) * 100
 
     # Affichage formaté
-    print(f"The dataframe includes {n_countries} countries.\n"
-          f"There are {n_obs} observations and {n_missing} missing values.\n"
-          f"The percentage of missing values for {var} in the dataset is {pct_missing:.2f}%.")
+    print(f"The dataframe includes {n_countries} countries and {n_missing} missing values.\n"
+          f"The percentage of missing values for the GDP per capita of {var} in the dataset is {pct_missing:.2f}%.")
