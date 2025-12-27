58216128726f6d5bfe1dfb77c7ad5088062b7502
tlaunay676 <thibaut.launay@ensae.fr>
2025-12-27 18:48:53 +0000

    Rajout Mortalité maternelle à la naissance

--- Main/PIB_hab.py
+++ Main/PIB_hab.py
@@ -81,4 +81,4 @@ def get_gdp_per_capita(start_year=2015, end_year=2024, indicator="NY.GDP.PCAP.CD
 
 df = get_gdp_per_capita_wide()
 
-df.to_csv("/home/onyxia/python-DATA/Données_PIB/Données_PIB_habitant_2015_2024.csv", index = False)
+df.to_csv("/home/onyxia/work/python-DATA/Données_PIB/Données_PIB_habitant_2015_2024.csv", index = False)
